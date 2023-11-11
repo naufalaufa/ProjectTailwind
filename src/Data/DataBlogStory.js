e658c1232ea938fb8d1d2bdf2956709d1aef1da5
naufal aufa <muhammadnaufalaufarifqi@gmail.com>
2023-11-11 21:58:54 +0700

    datablog

--- src/Data/DataBlogStory.js
+++ src/Data/DataBlogStory.js
@@ -1,19 +1,19 @@
 export const DataBlogStory = [
   {
     id: 1,
-    image: "src/assets/hujan.jpg",
+    image: "/assets/hujan.jpg",
     title: "Hujan Dan Kenangan",
     date: "11 november 2023",
   },
   {
     id: 2,
-    image: "src/assets/masaSMK.jpg",
+    image: "/assets/masaSMK.jpg",
     title: "Masa SMK",
     date: "11 november 2023",
   },
   {
     id: 3,
-    image: "src/assets/dirikuSurken.jpg",
+    image: "/assets/dirikuSurken.jpg",
     title: "Tersesat Di Surya Kencana",
     date: "11 November 2023",
   },
